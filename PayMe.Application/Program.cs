f952adb7b0b7774889f60919ce990df6f480c5bd
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-11-10 23:40:08 +0200

    Orginize project and initialize a basic structure of the project

--- PayMe.Application/Program.cs
+++ PayMe.Application/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PayMe.Application
+{
+    public class Create
+    {
+    }
+}
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './app/layout/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
