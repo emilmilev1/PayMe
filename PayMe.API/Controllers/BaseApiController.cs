efded2191c6b0f98b59fb0a3ae105a98f024aea7
Blhammer <emo.2012012@gmail.com>
2022-12-17 20:01:17 +0200

    Added Mediator to make tasks with the services

--- PayMe.API/Controllers/BaseApiController.cs
+++ PayMe.API/Controllers/BaseApiController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PayMe.Application.Core;
 
 namespace PayMe.API.Controllers
 {
@@ -9,6 +11,20 @@ namespace PayMe.API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
+        private IMediator? _mediator;
 
+        protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
+        
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null) return NotFound();
+
+            if (result.IsSuccess && result.Value != null) return Ok(result.Value);
+
+            if (result.IsSuccess && result.Value == null) return NotFound();
+
+            return BadRequest(result.Error);
+        }
     }
 }
