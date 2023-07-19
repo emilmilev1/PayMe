fb315f5e08d0a998bec41924c6c78365b59f94a7
Blhammer <emo.2012012@gmail.com>
2023-07-19 14:34:59 +0300

    Fix style 2

--- client-app/src/app/components/Confirm/ConfirmEmail.tsx
+++ client-app/src/app/components/Confirm/ConfirmEmail.tsx
@@ -45,7 +45,7 @@ const ConfirmEmail = () => {
         api.Account.resendEmailConfirm(email)
             .then(() => {
                 toast.success(
-                    "Verification email resent - please check your email"
+                    "Verification email resent - please check your email!"
                 );
             })
             .catch((error) => console.log(error));
